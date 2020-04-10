740b4c66251668361a79e83dabd9bf207235dbd0
tan-alu <786894966@qq.com>
2020-04-10 14:33:10 +0800

    没有更多数据可加载

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,8 @@ const goods= db.collection('goods')
 Page({
   data:{
     goods:[],
-    _page:0
+    _page:0,
+    hasMore:true//是否有更多数据可加载
   },
   onLoad(){
     this.loadListData()
@@ -21,11 +22,17 @@ Page({
     this.setData({
       // goods:[...goods,...res.data],
       goods:res.data,
-      _page:++_page
+      _page:++_page,
+      hasMore:res.data.length===LIMIT
     })
   },
   // 上拉刷新
   onReachBottom(){
+
+    // 没有更多数据的时候
+    if(!this.data.hasMore){
+      return console.log('没有更多数据了')
+    }
     console.log('shuaxin')
     this.loadListData()
   }
